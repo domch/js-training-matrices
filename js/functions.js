ce721fd9cdd3053ae1c3869e151e1400987038fa
Mehmet Dogan <info@intelligentworks.ch>
2017-09-18 21:37:40 +0200

    add multiplication solution

--- js/functions.js
+++ js/functions.js
@@ -32,3 +32,35 @@ function addMatrices(pMatrix1, pMatrix2){
 
 console.log(addMatrices(m1, m2));
 
+
+
+
+//The number of columns of the 1st matrix must equal the number of rows of the 2nd matrix.
+//And the result will have the same number of rows as the 1st matrix, and the same number of columns as the 2nd matrix.
+
+const result = [];                                // create result matrix
+const rowCount = m1.length;                       // number of rows in one of the matrix
+const colCount = m2[0].length;                    // number of columns in one of the matrix
+
+// check
+
+function getColumn(pMatrix, pCol){
+  return pMatrix.reduce((acc, value, index)=>{ 
+    acc.push(value[pCol]);
+    return acc;
+  }, [])
+}
+
+function multiplyLines(pRow, pCol){
+  return pRow.map((value, index) => value * pCol[index])
+      .reduce((acc, value) => acc + value, 0);
+}
+
+for (let row = 0; row < rowCount; row++) {
+  result.push([]);
+  for (let col = 0; col < colCount; col++) {
+    result[row][col]= multiplyLines(m1[row], getColumn(m2, col));
+  }
+}
+
+console.log(result);
